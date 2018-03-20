187f4630c8d077c365a7c19984a206db60fc02b0
Yuriy <yuabaranov@edu.hse.ru>
2018-03-20 09:29:51 +0300

    help option added

--- AsciiGraphics/src/Main.java
+++ AsciiGraphics/src/Main.java
@@ -6,39 +6,35 @@ import java.util.ArrayList;
 
 
 public class Main {
-
-    public static void main(String[] args) {
-        if (args.length == 0) {
-            throw new RuntimeException("file name must be passed as a first argument");
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0 || args[0].equals("--help") || args[0].equals("-h")) {
+            System.out.println("Single mode arguments: FILE");
+            System.out.println("Parallel mode arguments: FILE --parallel COUNT");
+            System.out.println("In parallel mode image must be a table with COUNT x COUNT digits");
+            return;
         }
 
-        BufferedImage source = null;
-        try {
-            source = ImageIO.read(new File(args[0]));
-        } catch (IOException e) {
-            System.out.print(e.getMessage());
-        }
-        
+        BufferedImage source = ImageIO.read(new File(args[0]));
         Jpeg2GrayScaleConverter converter = new Jpeg2GrayScaleConverter();
-        LinClassifier classifier = new LinClassifier("weights.txt");
+        LinClassifier classifier = new LinClassifier("data/weights.txt");
         
-        int imageSize = 28;
-        if (args.length == 1 || !args[1].equals("--parallel")) {
+        int imgSize = 28; // size required by classifier
+        if (args.length != 3 || !args[1].equals("--parallel")) {
             // jpeg to ASCII example, single digit classification
-            float[][] image = converter.convert(source, imageSize);
+            float[][] image = converter.convert(source, imgSize);
             GrayScaleAsciiPrinter.print(image);
             System.out.println(classifier.predict(image));
         } else {
             // parallel classification
-            int imagesCount = 10;
-            float[][] image = converter.convert(source, imagesCount * imageSize);
+            int imagesCount = Integer.valueOf(args[2]);
+            float[][] image = converter.convert(source, imagesCount * imgSize);
             int[][] ans = new int[imagesCount][imagesCount];
             ArrayList<Thread> workers = new ArrayList<>();
             for (int i = 0; i < imagesCount; i++) {
                 for (int j = 0; j < imagesCount; j++) {
                     final int x = j, y = i;
                     Thread worker = new Thread(() -> {
-                        float[][] chunk = ImageCutter.cut(image, x * imageSize, y * imageSize, imageSize, imageSize);
+                        float[][] chunk = ImageCutter.cut(image, x * imgSize, y * imgSize, imgSize, imgSize);
                         int prediction = classifier.predict(chunk);
                         synchronized(classifier) {
                             ans[y][x] = prediction;
