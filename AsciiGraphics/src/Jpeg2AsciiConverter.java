c94b66b181099e161273a9816725315ec7c11a51
Yuriy <yuabaranov@edu.hse.ru>
2018-03-20 03:28:19 +0300

    jpeg to ascii project added

---
+++ AsciiGraphics/src/Jpeg2AsciiConverter.java
@@ -0,0 +1,4 @@
+
+public class Jpeg2AsciiConverter {
+
+}
---
+++ AsciiGraphics/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            throw new RuntimeException("file name must be passed as a first argument");
+        }
+
+    }
+}
